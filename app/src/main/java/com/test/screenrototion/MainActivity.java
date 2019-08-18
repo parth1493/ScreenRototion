c36f7d6f5add395e4d7b26273f535d636919cf67
Parth <parth1493@gmail.com>
2019-08-18 01:19:09 -0400

    added logic for screen rotation.

--- app/src/main/java/com/test/screenrototion/MainActivity.java
+++ app/src/main/java/com/test/screenrototion/MainActivity.java
@@ -3,21 +3,46 @@ package com.test.screenrototion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getCanonicalName();
+    private EditText editText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Welcome "+editText.getText());
+                button.setText("Logout");
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.e(TAG , "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString("textview_text"));
+        button.setText(savedInstanceState.getString("button_text"));
     }
 
     @Override
@@ -30,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.e(TAG , "onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("textview_text",textView.getText().toString());
+        outState.putString("button_text",button.getText().toString());
     }
 
     @Override
